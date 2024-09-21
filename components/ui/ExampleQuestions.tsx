fa6d77bc51c55bcd5502e9c417bad1af82137038
Sato-07 <nivellesachraf@gmail.com>
2024-09-21 16:42:53 +0200

    feat: update

--- components/ui/ExampleQuestions.tsx
+++ components/ui/ExampleQuestions.tsx
@@ -20,7 +20,7 @@ const ExampleQuestions: React.FC<ExampleSentencesProps> = ({ onExampleSelect,sho
     show ?(
     <div className="mt-8">
       <h3>Exemples de phrases :</h3>
-      <ul className='mt-10 px-10'>
+      <ul className='mt-10 px-20'>
         {exampleSentences.map((sentence, index) => (
           <li key={index} className="cursor-pointer mb-7    flex flex-col items-center justify-center rounded-full font-medium
           bg-black text-light p-2 shadow-dark " onClick={() => handleSelectExample(sentence)}>
