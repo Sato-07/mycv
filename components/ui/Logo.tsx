9227b5c6e0b7653144fa77c8db4922150270ae9a
Sato-07 <nivellesachraf@gmail.com>
2024-09-23 21:47:36 +0200

    feat: update

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -1,15 +1,13 @@
-import Link from 'next/link'
-import React from 'react' 
-
+import Link from 'next/link';
+import React from 'react';
 
 export const Logo = () => {
   return (
     <div className='flex items-center justify-center mt-2'>
-        <Link href='/' className="font-mont font-semibold w-16  h-16 bg-dark text-light flex items-center  justify-center rounded-full border border-solid border-x-4 border-transparent dark:border-light dark:bg-light dark:text-dark">
+        <Link href='/' className="font-mont font-semibold w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full border border-solid border-x-4 border-transparent dark:border-light dark:bg-light dark:text-dark">
           AB
-          
         </Link>
     </div>
-  )
-}
+  );
+};
 
