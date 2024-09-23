67a67c2c804bbe531366a5dfd393919817290861
Sato-07 <nivellesachraf@gmail.com>
2024-09-23 21:42:45 +0200

    feat:update

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -7,6 +7,7 @@ export const Logo = () => {
     <div className='flex items-center justify-center mt-2'>
         <Link href='/' className="font-mont font-semibold w-16  h-16 bg-dark text-light flex items-center  justify-center rounded-full border border-solid border-x-4 border-transparent dark:border-light dark:bg-light dark:text-dark">
           AB
+          
         </Link>
     </div>
   )
